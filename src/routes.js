72becc22c8ce3785082590acf8e5e2e66310315e
Lucas Lamar <lucass.lamar@gmail.com>
2019-10-17 14:23:02 -0300

    I created loder of models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'teste' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'lucas lamar 2',
+        email: 'lucass.lamar2@hotmail.com',
+        password_hash: '1234567890',
+    });
+    return res.json(user);
 });
 
 export default routes;
