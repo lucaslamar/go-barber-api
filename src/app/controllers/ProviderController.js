c9dcc0e5052b37590a41a57984ae4f1384159127
Lucas Lamar <lucass.lamar@gmail.com>
2019-10-25 11:03:09 -0300

    pequenas correçoes de gitignore

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,9 @@
+import User from '../models/User';
+
+class ProviderController {
+    async index(req, res) {
+        const providers = await User.findAll({
+            where: { provider: true },
+        });
+    }
+}
