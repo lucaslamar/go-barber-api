a0febff21bd1a4d01c6d1bfcd7de4eadb4adfc7f
Lucas Lamar <lucass.lamar@gmail.com>
2019-11-07 12:30:54 -0300

    monitorando falhas na fila

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
     async index(req, res) {
@@ -136,23 +137,10 @@ class AppointmentController {
                     'You can only cancel appointments two hours ind advance.',
             });
         }
-        await Mail.sendMail({
-            to: `${appointment.provider.name} <${appointment.provider.email}>`,
-            subject: 'agendamento cancelado',
-            template: 'cancellation',
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                    "'dia' dd 'de' MMMM', às' H:mm'h'",
-                    {
-                        locale: pt,
-                    }
-                ),
-            },
-        });
 
+        await Queue.add(CancellationMail.key, {
+            appointment,
+        });
         return res.json(appointment);
     }
 }
